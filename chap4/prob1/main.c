c775718ee1995aa659f46fd73ed4bdfd9e5738b4
imjkr <119381643+imjkr@users.noreply.github.com>
2025-10-21 04:09:47 +0900

    Refactor file reading logic in main.c

--- chap4/prob1/main.c
+++ chap4/prob1/main.c
@@ -1,27 +1,20 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(int argc, char *argv[]) {
-    FILE *fp;
-    int ch;
+int main(int argc, char *argv[]){
+	FILE *fp;
+	int c;
+	
+	if(argc < 2)
+		fp = stdin;
+	else 
+		fp = fopen(argv[1], "r");
 
-    if (argc < 2) {
-        while ((ch = getchar()) != EOF) {
-            putchar(ch);
-        }
-    } else {
-        for (int i = 1; i < argc; i++) {
-            fp = fopen(argv[i], "r");
-            if (fp == NULL) {
-                perror(argv[i]);
-				continue;
-            }
-            while ((ch = fgetc(fp)) != EOF) {
-                putchar(ch);
-            }
-            fclose(fp);
-        }
-    }
-    return 0;
+	c = getc(fp);
+	while(c != EOF){
+		putc(c, stdout);
+		c = getc(fp);
+	}
+
+	fclose(fp);
+	return 0;
 }
-
